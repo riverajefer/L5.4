7979cb5efc4929a6ccc66374c3ef1cec9e1ca03a
riverajefer <jefersonpatino@yahoo.es>
2017-02-23 15:37:50 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
